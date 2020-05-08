975db3a00b5db68a7b95f228441853f44c5d50d7
Nimaoth <nicolas.otremba@web.de>
2020-05-08 22:21:57 +0200

    copy debugger to extension folder when building

--- build_debugger.ps1
+++ build_debugger.ps1
@@ -1,2 +1,6 @@
 &cheezc.exe src/debugger.che -o ./bin -n cheezdb --time --error-source --opt --print-linker-args
 # &cheezc.exe src/cli.che -o ./bin -n cheez --stdlib ./ --print-ast-analysed ./gen/int/ast_analyzed.che --emit-llvm-ir --time --error-source --opt
+
+if (Test-Path "$env:USERPROFILE\.vscode\extensions\cheez") {
+    Copy-Item ./bin/cheezdb.exe -Destination "$env:USERPROFILE\.vscode\extensions\cheez"
+}
