755861b157c90a54d1221b556dedaa39d7bda420
Nico <nicolas.otremba@web.de>
2020-09-07 17:28:05 +0200

    build should work on windows and linux

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-&cheezc.exe src/cli.che -o ./bin -n cheez --time --error-source --opt --print-linker-args --print-ast-analysed ./bin/int/ast_analyzed.chea
+&cheezc src/cli.che -o ./bin -n cheez --time --error-source --opt --print-linker-args --print-ast-analysed ./bin/int/ast_analyzed.chea
 # &cheezc.exe src/cli.che -o ./bin -n cheez --stdlib ./ --print-ast-analysed ./gen/int/ast_analyzed.che --emit-llvm-ir --time --error-source --opt
