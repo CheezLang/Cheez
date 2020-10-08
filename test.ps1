4ca0f6bd61551b913eb84755dfcf7fbcdf789333
Nimaoth <nicolas.otremba@web.de>
2020-10-08 21:32:23 +0200

    improved repl

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 Push-Location test
 # &../bin/cheez dump-tokens build.cheez
-&../bin/cheez build -thread_count 1
+# &../bin/cheez build -thread_count 1
 # &../bin/cheez repl -debug_jobs -debug_wait -debug_interpreter
-# &../bin/cheez repl
+&../bin/cheez repl
 Pop-Location
