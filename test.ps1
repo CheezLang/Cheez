30704f287948d14e428fa0e1b2f0c02e2b348155
Nimaoth <nicolas.otremba@web.de>
2020-10-08 20:00:00 +0200

    repl kindof working

--- test.ps1
+++ test.ps1
@@ -2,4 +2,5 @@ Push-Location test
 # &../bin/cheez dump-tokens build.cheez
 &../bin/cheez build -thread_count 1
 # &../bin/cheez repl -debug_jobs -debug_wait -debug_interpreter
+# &../bin/cheez repl
 Pop-Location
