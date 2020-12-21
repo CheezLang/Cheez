3b73e5da160d5bccebaf71300119f95b539d5888
Nimaoth <nicolas.otremba@web.de>
2020-12-21 13:45:59 +0100

    stuff

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
-&cheezc src/cli.che -o ./bin -n cheez --time --error-source --opt --print-linker-args --print-ast-analysed ./bin/int/ast_analyzed.chea
+&cheezc ../StandardLibrary/regex_test.che -o ./bin -n cheez --time --error-source --opt --print-linker-args --print-ast-analysed ./bin/int/ast_analyzed.chea
+# &cheezc src/cli.che -o ./bin -n cheez --time --error-source --opt --print-linker-args --print-ast-analysed ./bin/int/ast_analyzed.chea
 # &cheezc.exe src/cli.che -o ./bin -n cheez --stdlib ./ --print-ast-analysed ./gen/int/ast_analyzed.che --emit-llvm-ir --time --error-source --opt
--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 Push-Location test
-# &../bin/cheez dump-tokens build.cheez
-&../bin/cheez build -thread_count 1
+# &../bin/cheez dump-tokens io.cheez
+&../bin/cheez build
 # &../bin/cheez repl -debug_jobs -debug_wait -debug_interpreter
 # &../bin/cheez repl
 Pop-Location
